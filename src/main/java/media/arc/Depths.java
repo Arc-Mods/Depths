83fd291d68bf16184e1da167068d04387ee80e42
LittlePuffGD <littlepuffgd@gmail.com>
2025-06-19 00:32:04 -0400

    Updated a few things

--- src/main/java/media/arc/Depths.java
+++ src/main/java/media/arc/Depths.java
@@ -1,5 +1,6 @@
 package media.arc;
 
+import media.arc.world.biome.DepthsBiomes;
 import net.fabricmc.api.ModInitializer;
 
 import org.slf4j.Logger;
@@ -7,18 +8,12 @@ import org.slf4j.LoggerFactory;
 
 public class Depths implements ModInitializer {
 	public static final String MOD_ID = "depths";
-
-	// This logger is used to write text to the console and the log file.
-	// It is considered best practice to use your mod id as the logger's name.
-	// That way, it's clear which mod wrote info, warnings, and errors.
 	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
 
 	@Override
 	public void onInitialize() {
-		// This code runs as soon as Minecraft is in a mod-load-ready state.
-		// However, some things (like resources) may still be uninitialized.
-		// Proceed with mild caution.
+		DepthsBiomes.registerBiomes();
 
-		LOGGER.info("Hello Fabric world!");
+		LOGGER.info("Dredge the depths.");
 	}
 }
